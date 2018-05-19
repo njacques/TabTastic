82f66baf6821d1792c62ebad8bcb9ce2852f00fe
Nilson Jacques <neil.jacques@gmail.com>
2018-05-19 16:43:01 -0300

    Add Bulma template and display tabs with favicons

--- src/js/background.js
+++ src/js/background.js
@@ -2,14 +2,14 @@
 
 // See: https://chromium.googlesource.com/chromium/src/+/master/chrome/common/extensions/docs/examples/api/tabs/screenshot/
 
-import '../img/icon-128.png';
-import '../img/icon-34.png';
-import { nonInternalTabs, parseUrl } from './helpers/helpers';
+import "../img/icon-128.png";
+import "../img/icon-34.png";
+import { nonInternalTabs } from "./helpers/helpers";
 
-const kommanderUrl = chrome.runtime.getURL('kommander.html');
+const kommanderUrl = chrome.runtime.getURL("kommander.html");
 
 chrome.browserAction.onClicked.addListener(() => {
-  chrome.tabs.query({}, (tabs) => {
+  chrome.tabs.query({}, tabs => {
     const tabList = tabs
       .filter(tab => tab.url !== kommanderUrl)
       .filter(nonInternalTabs);
@@ -20,7 +20,10 @@ chrome.browserAction.onClicked.addListener(() => {
     chrome.storage.local.set({ tabs: tabList }, () => {
       if (komanderIsOpen) {
         const kommanderTab = tabs.filter(tab => tab.url === kommanderUrl)[0];
-        chrome.tabs.update(kommanderTab.id, { active: true, url: kommanderUrl });
+        chrome.tabs.update(kommanderTab.id, {
+          active: true,
+          url: kommanderUrl
+        });
       } else {
         chrome.tabs.create({ url: kommanderUrl });
       }
--- src/js/kommander.js
+++ src/js/kommander.js
@@ -1,12 +1,42 @@
-/* global document window */
+/* global document window chrome */
+import parseDomain from "parse-domain";
+import "../css/kommander.css";
+import { parseUrl } from "./helpers/helpers";
 
-import { parseUrl } from './helpers/helpers';
+const defaultFavicon =
+  "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAZ0lEQVQ4T2NkoBAwIuuPiIhwwGHegxUrVjzAJodhwIoVKw6gG/r///8fjIyML7AZQpQBL168OCEuLm6AzRCiDAC5ysHBgQObIUQZgO53ZG8SNABdMyigRw0Y/mFATObEmQ6I0YyuBgB6/3gRnZbduwAAAABJRU5ErkJggg==";
 
-chrome.storage.local.get(['tabs'], ({ tabs }) => {
-  const tabHTML = tabs
-    .map(tab => `<li><a href="${parseUrl(tab.url)}" target="blank">${tab.title}</a></li>`)
-    .join('\n');
+window.inbox = [];
 
-  document.getElementById('tab-count').innerHTML = tabs.length;
-  document.getElementById('target').innerHTML = tabHTML;
+const getDomain = url => {
+  const parts = parseDomain(url);
+  return parts ? `${parts.domain}.${parts.tld}` : "unknown";
+};
+
+const renderTab = tab => `
+  <div class="card">
+    <div class="card-content-modified">
+      <div class="msg-subject ">
+        <span class="msg-subject ">
+          <img src="${tab.favIconUrl ||
+            defaultFavicon}" height="16" width="16" />
+          ${tab.title} (${getDomain(tab.url)})
+        </span>
+      </div>
+    </div>
+  </div>
+`;
+
+chrome.storage.local.get(["tabs"], ({ tabs }) => {
+  const inbox = tabs
+    .map(({ id, favIconUrl, title, url }) => ({
+      id,
+      favIconUrl,
+      title,
+      url: parseUrl(url)
+    }))
+    .map(renderTab)
+    .join("\n");
+
+  document.getElementById("inbox-messages").innerHTML = inbox;
 });
